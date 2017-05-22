5fe9bf4d92d22e332056c660c5036afb954f1ece
knerushkin <knerushkin@gmail.com>
2017-05-23 00:29:17 +0200

    Added Factor Graph. Uploading .cnf files from local storage. Tab view on different tye of graphs.

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from __future__ import print_function
-from flask import Flask, render_template
+
+import urllib2
+
+from flask import Flask, render_template, request, redirect, url_for
 import os
 import socket
 import json
 from utils import problem
 from subprocess import call
+import pycosat
+from utils.problem import sign
 
 app = Flask(__name__)
-
+UPLOAD_FOLDER = 'static/data'
+ALLOWED_EXTENSIONS = set(['cnf'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>"
     return html.format(name=os.getenv('NAME', "world"), hostname=socket.gethostname())
-
-
-@app.route("/sat/data")
-def data():
-    return json.dumps(problem.read("dubois20.cnf"))
 
 
 @app.route("/load", methods=['GET', 'POST'])
@@ -34,13 +36,18 @@ def load():
     '''
 
 
+
+
+
 @app.route("/interaction")
 def interactions():
-    cnf = problem.Problem(dimacs_file_path)
+    print(dimacs_file_path[2])
+    cnf = problem.Problem(dimacs_file_path[2])
     problem.generate_interaction_graph(cnf)
     return render_template('interaction.html',
                             cnf=cnf,
                             graph_json='interaction_graph.json')
+
 
 
 @app.route("/interaction/satelited")
@@ -53,7 +60,87 @@ def interactions_satelited():
                             graph_json='interaction_graph_satelited.json')
 
 
+@app.route("/sat/data/satelited")
+def data_satelited():
+    problem.satelite_it(dimacs_file_path)
+    return json.dumps(problem.read("bin/dubois20.cnf"))
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+def list_data(path):
+    l = []
+    for folder, subfolder, file in os.walk(path):
+        print(file)
+        l.extend(file)
+
+    return l
+
+
+@app.route("/visualisation", methods=['GET', 'POST'])
+def show():
+    selected = request.args.get('file')
+    file_list = list_data('static/data')
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('show'))
+
+    return render_template("visualisation.html", file_list=file_list, selected=selected)
+
+
+@app.route("/sat/solve/<file>")
+def solve(file):
+    print(file)
+    solution = pycosat.solve(problem.read('static/data/' + file)["clauses"])
+    result = []
+    if solution != "UNSAT":
+        for value in solution:
+            result.append({"id": "L" + str(abs(value)) + "",
+                           "value":  sign(value)})
+    else:
+        result = solution
+    return json.dumps(result)
+
+
+
+@app.route("/visual/repr/factor/<file>")
+def graph(file):
+    return json.dumps(problem.transform(problem.read("static/data/" + file)))
+
+@app.route("/visual/repr/interaction/<file>")
+def graph_interaction(file):
+    return json.dumps(problem.generate_interaction_graph(problem.Problem("static/data/" + file)))
+
+@app.route("/solvers")
+def solvers():
+    print(urllib2.urlopen("http://baldur.iti.kit.edu/sat-competition-2016/solvers/main/").read())
+    print(graph())
+    return '''
+        <!doctype html>
+        <title>Solvers</title>
+        <h1>Solvers</h1>
+        '''
+
+@app.route("/visual/repr/satelited")
+def graph_satelited():
+    problem.satelite_it(dimacs_file_path)
+    cnf_satelited = problem.Problem('bin/pre-satelited.cnf')
+    return json.dumps(problem.transform(problem.read("bin/dubois20.cnf")))
+
+
 if __name__ == "__main__":
-    dimacs_file_path = 'bin/dubois20.cnf'
+    dimacs_file_path = ["bin/dubois20.cnf", "bin/aim-100-1_6-no-1.cnf", "bin/par8-1-c.cnf"]
+    print(problem.read(dimacs_file_path[1])["clauses"])
+    cnf = [[1, -3], [2, 3, -1]]
+    print(pycosat.solve(problem.read(dimacs_file_path[1])["clauses"]))
+
+    print(pycosat.solve(problem.read(dimacs_file_path[2])["clauses"]))
+    print(pycosat.solve(problem.read(dimacs_file_path[0])["clauses"]))
+    print(pycosat.solve(cnf))
     app.run()
 
--- utils/problem.py
+++ utils/problem.py
@@ -2,7 +2,7 @@ from mxklabs import dimacs
 import json
 from sys import platform as _platform
 from subprocess import call
-
+import os.path
 
 class Problem(object):
     interaction_variables = {}
@@ -52,25 +52,80 @@ def generate_interaction_graph(problem):
     interaction_graph = dict(nodes=[], links=[])
     for v, i in problem.interaction_variables.iteritems():
         node = dict(id=str(v), group=1)
-        interaction_graph['nodes'].append(node)
+        interaction_graph["nodes"].append(node)
         links = [dict(source=str(v), target=str(item), weight=1) for item in i]
-        interaction_graph['links'].extend(links)
+        print(links)
+        interaction_graph["links"].extend(links)
 
     if problem.satelited:
         graph_file_path = 'static/interaction_graph_satelited.json'
     else:
         graph_file_path = 'static/interaction_graph.json'
-
-    with open(graph_file_path, 'w') as outfile:
-        json.dump(interaction_graph, outfile)
-
+    #
+    # with open(graph_file_path, 'w') as outfile:
+    #     json.dump(interaction_graph, outfile)
+    print(json.dumps(interaction_graph))
+    return interaction_graph
 
 def satelite_it(dimacs_file_path):
+    satelite_path = ''
+
     if _platform == "linux" or _platform == "linux2":
         satelite_path = 'bin/SatELite_v1.0_linux'
+
     elif _platform == "darwin":
-        satelite_path = 'bin/SatELite_v1.macOS'
+        satelite_path = 'bin/SatELite_v1.0_macOS'
+
+    print(satelite_path + 'is exist: ' + str(os.path.isfile(satelite_path)))
+    print('../' + dimacs_file_path + 'is exist: ' + str(os.path.isfile( dimacs_file_path)))
 
     flags = '+pre'
     call([satelite_path, dimacs_file_path, flags])
     call(['mv', 'pre-satelited.cnf', 'bin'])
+
+
+# author: knerushkin@gmail.com
+def transform(data):
+    result = {}
+    result.update({"nodes": nodes(data["clauses"], data["num_clauses"], data["num_vars"])})
+    result.update({"links": links(data["clauses"])})
+    return result
+
+
+def sign(x):
+    return 'positive' if x > 0 else 'negative'
+
+
+def link(clause, count):
+    result = []
+    print(clause)
+    for literal in clause:
+        print(literal)
+        result.append({"source": "L{l}".format(l=abs(literal)), "target": "C{c}".format(c=count), "value": 1,
+                       "direction": sign(literal)})
+
+    return result
+
+
+def links(clauses):
+    links = []
+    for count, clause in enumerate(clauses):
+        links.append(link(clause,count))
+
+    return [item for sublist in links for item in sublist]
+
+
+def nodes(clauses, num_clauses, num_literals):
+    result = []
+    for clause in range(0, num_clauses):
+        result.append({"id": "C" + str(clause), "group": "clause"})
+
+    for literal in range(1, num_literals + 1):
+        result.append({"id": "L" + str(abs(literal)) + "",
+                       "group":  "literal"})
+    return result
+
+
+if __name__ == "__main__":
+    data = read("../bin/dubois20.cnf")
+    print(data)
